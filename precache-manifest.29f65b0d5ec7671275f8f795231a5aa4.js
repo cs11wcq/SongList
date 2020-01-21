ba3ecb50ae5e706e71b3d29f78ddea96112a1f2b
sethy <syk106@ucsd.edu>
2020-01-21 14:49:21 -0800

    Updates

---
+++ precache-manifest.29f65b0d5ec7671275f8f795231a5aa4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aca642d933691ccdfe1bf3a468b8ba6",
+    "url": "/SongList/index.html"
+  },
+  {
+    "revision": "fdeff4fd66817c0c352e",
+    "url": "/SongList/static/js/2.3e9c0c66.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/SongList/static/js/2.3e9c0c66.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a99bf10b030ca8d1f73d",
+    "url": "/SongList/static/js/main.a5da8128.chunk.js"
+  },
+  {
+    "revision": "9950f93eb3c02dd81860",
+    "url": "/SongList/static/js/runtime-main.a35dc3d1.js"
+  }
+]);
